912023aefa507e7296de616beeea99d588ffb686
ankurdome <ankurdome02@gmail.com>
2025-11-16 03:06:44 +0530

    Initial commit: CI/CD with Jenkins and Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]  # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name      = "MacBookAir"
+
+  tags = {
+    Name = "Terraform-Jenkins-EC2"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0c02fb55956c7d316"  # Ubuntu 22.04 for ap-south-1
+}
